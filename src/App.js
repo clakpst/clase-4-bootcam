58a16589621263a1922f95fabd27da6884c21aeb
clakpst3 <117895519+clakpst@users.noreply.github.com>
2025-02-19 10:32:55 +0000

    aaa

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+import { useState } from "react";
 import Nota from "./Note";
-const notes = [
-  {
-    id: 13,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 27,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 38,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
-export default function App() {
+export default function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    const noteToAddToState = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random <= 0.5,
+    };
+    setNotes([...notes, noteToAddToState]);
+    setNewNote("");
+  };
   return (
     <div>
+      <h1>Notas</h1>
       {notes.map((note) => {
         return <Nota content={note.content} key={note.id} />;
       })}
+      <div>
+        <input type="text" onChange={handleChange} value={newNote} />
+        <button onClick={handleClick}>Crear nota</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App notes={notes} />);
